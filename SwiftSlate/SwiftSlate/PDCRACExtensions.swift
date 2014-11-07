ecc38aef8ecdda702be4e69fa18e56b4ceebdfa2
Alex Ehlke <alex.ehlke@gmail.com>
2014-11-07 00:27:06 -0500

    use AshFurrow's RAC macros

--- SwiftSlate/SwiftSlate/PDCRACExtensions.swift
+++ SwiftSlate/SwiftSlate/PDCRACExtensions.swift
@@ -1,37 +1,37 @@
-//
-//  PDCRACExtensions.swift
-//  SwiftSlate
-//
-//  Created by Kyle Oba on 8/5/14.
-//  Copyright (c) 2014 Pas de Chocolat. All rights reserved.
-//
-/// Extensions for RAC based on:
-/// http://napora.org/a-swift-reaction/
-/// http://www.scottlogic.com/blog/2014/07/24/mvvm-reactivecocoa-swift.html
-
 import Foundation
+import ReactiveCocoa
 
-struct RAC  {
-    var target : NSObject!
-    var keyPath : String!
-    var nilValue : AnyObject!
+// So I expect the ReactiveCocoa fellows to figure out a replacement API for the RAC macro.
+// Currently, I don't see one there, so we'll use this solution until an official one exists.
+
+// Pulled from http://www.scottlogic.com/blog/2014/07/24/mvvm-reactivecocoa-swift.html
+
+public struct RAC  {
+    var target: NSObject
+    var keyPath: String
+    var nilValue: AnyObject?
     
-    init(_ target: NSObject!, _ keyPath: String, nilValue: AnyObject? = nil) {
+    public init(_ target: NSObject, _ keyPath: String, nilValue: AnyObject? = nil) {
         self.target = target
         self.keyPath = keyPath
         self.nilValue = nilValue
     }
     
-    func assignSignal(signal : RACSignal) {
-        signal.setKeyPath(self.keyPath, onObject: self.target, nilValue: self.nilValue)
+    func assignSignal(signal : RACSignal) -> RACDisposable {
+        return signal.setKeyPath(self.keyPath, onObject: self.target, nilValue: self.nilValue)
     }
 }
 
-infix operator ~> {}
-func ~> (signal: RACSignal, rac: RAC) {
-    rac.assignSignal(signal)
+infix operator <~ {}
+public func <~ (rac: RAC, signal: RACSignal) -> RACDisposable {
+    return signal ~> rac
 }
 
-func RACObserve(target: NSObject!, keyPath: String) -> RACSignal  {
+public func ~> (signal: RACSignal, rac: RAC) -> RACDisposable {
+    return rac.assignSignal(signal)
+}
+
+public func RACObserve(target: NSObject!, keyPath: String) -> RACSignal {
     return target.rac_valuesForKeyPath(keyPath, observer: target)
 }
+
